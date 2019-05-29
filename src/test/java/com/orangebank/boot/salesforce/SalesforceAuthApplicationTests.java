a4bbaa74b9a5d45cf3c5b56cd993ff10fcc9b6a0
Esteban Martín-Tembleque Poves <estebanmate@gmail.com>
2019-05-29 16:47:48 +0200

    Configuration
    Configuration

---
+++ src/test/java/com/orangebank/boot/salesforce/SalesforceAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orangebank.boot.salesforce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesforceAuthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
